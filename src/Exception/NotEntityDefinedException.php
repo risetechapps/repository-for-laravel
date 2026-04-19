ab08e989654eb629333fd0ad37a7f417b28624ab
risetechapps <apps@risetech.com.br>
2026-04-19 18:28:52 -0300

    feat: adicionado suporte a exceções específicas
    RepositoryException - Classe base.
    EntityNotFoundException - Entidade não encontrada.
    InvalidFilterException - Filtro inválido.
    CacheOperationException - Erro de cache.
    MaterializedViewException - Erro de view materializada.
    NotEntityDefinedException atualizado com mensagens.

--- src/Exception/NotEntityDefinedException.php
+++ src/Exception/NotEntityDefinedException.php
@@ -1,10 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace RiseTechApps\Repository\Exception;
 
-use Exception;
-
-class NotEntityDefinedException extends Exception
+class NotEntityDefinedException extends RepositoryException
 {
+    public function __construct(string $repositoryClass = '')
+    {
+        $message = $repositoryClass !== ''
+            ? "O método entity() não foi definido no repository [{$repositoryClass}]."
+            : "O método entity() não foi definido no repository.";
 
+        parent::__construct($message, 500, null, [
+            'repository' => $repositoryClass,
+        ]);
+    }
 }
